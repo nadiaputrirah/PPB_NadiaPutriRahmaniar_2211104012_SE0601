8def4bb2e53ccfe5589700ad9fd84d4eaa0b4667
nadiaputrirah <nadiapputriirah@gmail.com>
2024-10-22 19:21:25 +0700

    task andvance layout

---
+++ PPB_Teori/pertemuan_05/android/app/src/main/kotlin/com/example/pertemuan_05/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pertemuan_05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ PPB_Teori/pertemuan_05/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Andvance Layout',
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  PageController _pageController = PageController();
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          children: [
+            // Halaman pertama
+            Container(
+              child: Center(
+                child: Text(
+                  "Home page",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+            // Halaman kedua
+            Container(
+              child: Center(
+                child: Text(
+                  "Email page",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+            // Halaman ketiga
+            Container(
+              child: Center(
+                child: Text(
+                  "Profile page",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.email),
+            label: 'Email',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        backgroundColor: Colors.orange,
+      ),
+    );
+  }
+}
